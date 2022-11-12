3c9a9ae05cdf324ce3767ba030aae45a9e74a7b8
Blake Frederick <bf@six12creative.com>
2022-11-11 21:35:15 -0800

    What's happening?

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+'use client'
+
+import { useState} from 'react'
 
 function ChatInput() {
+    const [input, setInput] = useState('')
+
   return (
     <form className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100">
-        <input type="input" placeholder="What's happening?" className="flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 diabled:cursor-not-allowed"></input>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed" 
-        >Send</button>
+        <input 
+            type="input" 
+            placeholder="What's happening?" 
+            onChange={(e) => setInput(e.target.value)}
+            className="flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 diabled:cursor-not-allowed"></input>
+        <button 
+            type="submit" 
+            disabled={!input}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed" 
+        >
+            Send</button>
     </form>
   )
 }
