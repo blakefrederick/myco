f938b8d02b635bdcac0c982711a64c9cd765ecd8
Blake Frederick <bf@six12creative.com>
2022-11-11 21:32:48 -0800

    go go tailwind

--- app/ChatInput.tsx
+++ app/ChatInput.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function ChatInput() {
   return (
-    <form>
-        <input type="input"></input>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" 
-        type="submit">Send</button>
+    <form className="fixed bottom-0 z-50 w-full flex px-10 py-5 space-x-2 border-t border-gray-100">
+        <input type="input" placeholder="What's happening?" className="flex-1 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent px-5 py-3 disabled:opacity-50 diabled:cursor-not-allowed"></input>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed" 
+        >Send</button>
     </form>
   )
 }
