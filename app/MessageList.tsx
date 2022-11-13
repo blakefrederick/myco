27e6bf6aa61673088425a35ef6570aa4e60c771a
Blake Frederick <bf@six12creative.com>
2022-11-12 16:24:40 -0800

    Display all messages

--- app/MessageList.tsx
+++ app/MessageList.tsx
@@ -1,14 +1,24 @@
+'use client'
+
 import React from 'react'
+import { Message } from '../typings'
+import useSWR from 'swr'
+import fetcher from '../utils/fetchMessages'
 
 function MessageList() {
+  const {
+    data: messages,
+    error,
+    mutate,
+  } = useSWR<Message[]>('/api/getMessages', fetcher)
+
   return (
-    <div>
-      <p>Hey don't check out this message</p>
-      <p>Hey check out this message</p>
-      <p>Hey check out this message</p>
-      <p>Hey check out this message</p>
-      <p>Hey check out this message</p>
-      <p>Hey check out this message</p>
+    <div className="p-5">
+      {messages?.map((message) => (
+        <div key={message.id}>
+          <p className="m-3">{message.message}</p>
+        </div>
+      ))}
     </div>
   )
 }
