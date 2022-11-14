3ee6928ec922f30364ad1ee3ab108c8d379badf8
Blake Frederick <bf@six12creative.com>
2022-11-13 21:37:24 -0800

    Facebook sign out

--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -1,12 +1,22 @@
 'use client'
 
 import React, { useState } from 'react'
+import { signOut } from 'next-auth/react'
 
 function LogoutButton() {
   const [buttonText, setButtonText] = useState('Logout')
+
+  const processSignOut = () => {
+    if (buttonText !== 'No') {
+      setButtonText('No')
+    } else {
+      setButtonText('Okay! Signed Out!')
+      signOut()
+    }
+  }
   return (
     <button
-      onClick={() => setButtonText('No')}
+      onClick={() => processSignOut()}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
       {buttonText}
