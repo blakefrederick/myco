4cc47ba3df76bcf89e022a8b2409db6e13dea184
Blake Frederick <bf@six12creative.com>
2022-11-16 00:03:22 -0800

    Add sign in with Spotify

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth'
 import FacebookProvider from 'next-auth/providers/facebook'
 import GitHubProvider from 'next-auth/providers/github'
 import TwitterProvider from 'next-auth/providers/twitter'
+import SpotifyProvider from 'next-auth/providers/spotify'
 
 export const authOptions = {
   providers: [
@@ -22,6 +23,10 @@ export const authOptions = {
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
       version: '2.0', // opt-in to Twitter OAuth 2.0
+    }),
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET!,
