045c113ea94855892060407a023e9ce24b5b36e2
Blake Frederick <bf@six12creative.com>
2022-11-15 23:38:42 -0800

    Add Twitter login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
 import FacebookProvider from 'next-auth/providers/facebook'
 import GitHubProvider from 'next-auth/providers/github'
+import TwitterProvider from 'next-auth/providers/twitter'
 
 export const authOptions = {
   providers: [
@@ -16,6 +17,11 @@ export const authOptions = {
           scope: 'read:user user:email user:follow',
         },
       },
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+      version: '2.0', // opt-in to Twitter OAuth 2.0
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET!,
