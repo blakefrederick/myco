340665a492768a427a12ab18bb983ac70c4958d4
Blake Frederick <bf@six12creative.com>
2022-11-16 20:30:40 -0800

    builds and works now

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ import { unstable_getServerSession } from 'next-auth/next'
 import { Providers } from './providers'
 
 async function HomePage() {
-  const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then(
-    (res) => res.json()
-  )
+  // const data = await fetch(`${process.env.VERCEL_URL}/api/getMessages`).then(
+  //   (res) => res.json()
+  // )
 
-  const messages: Message[] = data.messages // this and fetch unused because it's not passed to MessageList
+  // const messages: Message[] = data.messages // this and fetch unused because it's not passed to MessageList
   const session = await unstable_getServerSession()
   console.log('session', session)
 
