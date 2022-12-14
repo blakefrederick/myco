7e780b704d5358c5954994f58c16fa7a04c48c26
Blake Frederick <bf@six12creative.com>
2022-12-13 23:19:06 -0800

    Heart click routes

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,12 +1,18 @@
+'use client'
+
 import { Providers } from '../providers'
 import MessageList from '../MessageList'
+import { useRouter } from 'next/navigation'
 
-async function PreviewPage() {
+function PreviewPage() {
+  const router = useRouter()
   return (
     <Providers session={''}>
       <main>
-        {<MessageList />}
-        <h1 className="text-center">&#9825;</h1>
+        <MessageList />
+        <h1 className="text-center" onClick={() => router.push('/about')}>
+          &#9825;
+        </h1>
       </main>
     </Providers>
   )
